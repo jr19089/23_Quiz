bc5f52156c5c7d59ad4d275432d9ba204bbe42ab
jr19089 <jr19089@melville-high.school.nz>
2023-08-16 12:42:48 +1200

    complete component, use later

--- 01_GUIz.py
+++ 01_GUIz.py
@@ -4,34 +4,35 @@ from random import shuffle
 
 class Converter:
     def __init__(self):
-        self.temp_frame = Frame(padx=15, pady=15, bg='dodger blue')
+        self.temp_frame = Frame(padx=15, pady=15, bg='forest green')
         self.temp_frame.grid()
         self.temp_heading = Label(self.temp_frame,
                                   text="Welcome To The Quiz",
                                   font=("Terminal", "20", "bold"),
-                                  bg='dodger blue')
+                                  bg='forest green')
         self.temp_heading.grid(row=0)
 
         explanation = "This quiz will test your knowledge of pop culture"
         self.temp_explanation = Label(self.temp_frame, text=explanation, font='system',
-                                      bg='dodger blue')
+                                      bg='forest green')
         self.temp_explanation.grid(row=1)
 
         explanation2 = "and your ability to make educated guesses."
         self.temp_explanation = Label(self.temp_frame, text=explanation2, font='system',
-                                      bg='dodger blue')
+                                      bg='forest green')
         self.temp_explanation.grid(row=2)
 
         stakes = "You have 3 Lives. Make them count."
         self.temp_explanation = Label(self.temp_frame, text=stakes, font='system',
-                                      bg='dodger blue', fg='red')
+                                      bg='forest green', fg='red')
         self.temp_explanation.grid(row=3)
 
-        self.button_frame = Frame(self.temp_frame)
+        self.button_frame = Frame(self.temp_frame, bg='forest green')
         self.button_frame.grid(row=4)
 
         self.begin = Button(self.button_frame,
-                            )
+                            text="Start", bg='deep pink', font=('system', 25))
+        self.begin.grid(row=0, column=0, padx=15, pady=15)
 
 
 # main routine
